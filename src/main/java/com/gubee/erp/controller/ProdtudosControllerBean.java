67ea109a176a6b957998d84333621f941604d3e2
RomarioRibeiro <romaryjane@hotmail.com>
2023-03-20 17:52:46 -0300

    adicionando controller

---
+++ src/main/java/com/gubee/erp/controller/ProdtudosControllerBean.java
@@ -0,0 +1,14 @@
+package com.gubee.erp.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class ProdtudosControllerBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
--- target/classes/META-INF/sql/dados-iniciais.sql
+++ target/classes/META-INF/sql/dados-iniciais.sql
@@ -1,10 +1,10 @@
-insert into produto (id, descricao) values (1, 'Curso em Java');
-insert into produto (id, descricao) values (2, 'Curso de REST API');
-insert into produto (id, descricao) values (3, 'ANGULAR');
-insert into produto (id, descricao) values (4, 'HTML E CSS');
+insert into produto (id, nome, descricao) values (1 ,'Curso em Java', 'Curso');
+insert into produto (id, nome, descricao) values (2 ,'Curso de Angular', 'Curso');
+insert into produto (id, nome, descricao) values (3 ,'Curso de REST API', 'Curso');
+insert into produto (id, nome, descricao) values (4 ,'Curso de HTML CSS', 'Curso');
 
 
 insert into mercado (id, nome, produto_id) values (1, 'Java', 1);
-insert into mercado (id, nome, produto_id) values (1, 'REST', 2);
-insert into mercado (id, nome, produto_id) values (1, 'Angular', 3);
-insert into mercado (id, nome, produto_id) values (1, 'HTML CSS', 4);
+insert into mercado (id, nome, produto_id) values (2, 'REST', 3);
+insert into mercado (id, nome, produto_id) values (3, 'Angular', 2);
+insert into mercado (id, nome, produto_id) values (4, 'HTML CSS', 4);
